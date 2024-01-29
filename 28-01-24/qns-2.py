406a19e02b69d314c67026eb4beaeffd3a6603d9
Priyavelayudham <157309725+Priyavelayudham@users.noreply.github.com>
2024-01-29 03:48:29 -0800

    Create qns-2.py

---
+++ 28-01-24/qns-2.py
@@ -0,0 +1,35 @@
+"""Given five positive integers, find the minimum and maximum values that can be calculated by summing exactly four of the five integers. Then print the respective minimum and maximum values as a single line of two space-separated long integers.
+
+Example
+
+The minimum sum is  and the maximum sum is . The function prints
+
+16 24"""
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'miniMaxSum' function below.
+#
+# The function accepts INTEGER_ARRAY arr as parameter.
+#
+
+def miniMaxSum(arr):
+    # Write your code here
+    sum=0
+
+    for i in range(len(arr)):
+
+        sum=sum+arr[i]
+
+    print ( sum-max(arr), sum-min(arr))
+if __name__ == '__main__':
+
+    arr = list(map(int, input().rstrip().split()))
+
+    miniMaxSum(arr)
